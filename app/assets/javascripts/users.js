1dfcd306ac158e4cc7be6e86ff22752002f1a298
Patrick Matthies <matthies.patrick.d@gmail.com>
2016-12-17 20:53:30 +0000

    Added Stripe response handler and error handling js

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -11,6 +11,7 @@ $(document).on('turbolinks:load', function(){
   submitBtn.click(function(event){
       // prevent default subission behavor.
     event.preventDefault();
+    submitBtn.val("Processing").prop('disabled', true);
     
     // Collect the credit card fields.
     var ccNum = $('#card_number').val(),
@@ -18,18 +19,52 @@ $(document).on('turbolinks:load', function(){
         expMonth = $('#card_month').val(),
         expYear = $('#card_year').val();
         
-    // Send the card info to Stripe.
-    Stripe.createToken({
-      number: ccNum,
-      cvc: cvcNum,
-      exp_month: expMonth,
-      exp_year: expYear
-    }, stripeResponseHandler);
+    // Use stripe.js library to check for card errors
+    var error = false;
+    
+    // Validate card number.
+    if (!Stripe.card.validateCardNumber(ccNum) ){
+      error = true;
+      alert('The credit card number appears to be invalid');
+    }
+    
+    // Validate cvc number.
+    if (!Stripe.card.validateCVC(cvcNum) ){
+      error = true;
+      alert('The cvc number appears to be invalid');
+    }
+   
+   // Validate expiration date.
+    if (!Stripe.card.validateExpiry(expMonth, expYear) ){
+      error = true;
+      alert('The expiration date appears to be invalid');
+    } 
+   
+    if (error) {
+      // If there are card errors, dont send to Stripe.
+      submitBtn.prop('disabled', false).val("Sign Up")
+    } else {
+      // Send the card info to Stripe.
+      Stripe.createToken({
+        number: ccNum,
+        cvc: cvcNum,
+        exp_month: expMonth,
+        exp_year: expYear
+      }, stripeResponseHandler);
+    }
+    
+    return false;
   });
-
   
-
-  // Stripe will return a card token.
-  // Inject card token as hidden field into form.
-  // Submit for to out Rails app.
+  // Stripe will return a card token.  
+  function stripeResponseHandler(status, response) {
+    // Get the token from the response.
+    var token = response.id;
+    
+    // Inject the card token in a hidden field.
+    theForm.append( $('<input type="hidden" name="user[stripe_card_token]">').val(token) );
+    
+    // Submit form to our Rails app - get first array position from form id.
+    theForm.get(0).submit();
+  }
 });
